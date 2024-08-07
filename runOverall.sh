ef81030a6e1999647de1198a960ed648f7a7d5dd
annieyii <annannann0710@gmail.com>
2024-08-07 15:45:00 +0800

    上傳圖片時圖片會被存在資料夾中而不是之前零散的樣子

--- runOverall.sh
+++ runOverall.sh
@@ -4,12 +4,19 @@
 file1=$(sed -n '1p' filenames.txt)
 file2=$(sed -n '2p' filenames.txt)
 
-mv uploads/$file1 code/
-mv uploads/$file2 code/
+# Generate a random directory name
+random_dir=$(mktemp -d dir_XXXXXX)
+touch imagefileName.txt
+mv $random_dir code/
+
+# Insert the random directory name into the third line of filenames.txt
+echo $random_dir > imagefileName.txt
+
+mv uploads/$file1 code/$random_dir/
+mv uploads/$file2 code/$random_dir/
 
 # Call the Python script to compare the images
-pip3 install -r requirement.txt
-python3 code/hsv_similarity.py $file1 $file2 > HSVresult.txt
-python3 code/ssim.py $file1 $file2 > SSIMresult.txt
-python3 code/yolo.py code/$file1 > YOLOresult1.txt 
-python3 code/yolo.py code/$file2 > YOLOresult2.txt
+python3 code/hsv_similarity.py $random_dir/$file1 $random_dir/$file2 > HSVresult.txt
+python3 code/ssim.py $random_dir/$file1 $random_dir/$file2 > SSIMresult.txt
+python3 code/yolo.py code/$random_dir/$file1 > YOLOresult1.txt 
+python3 code/yolo.py code/$random_dir/$file2 > YOLOresult2.txt
